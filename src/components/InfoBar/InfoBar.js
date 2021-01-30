5de74a3f42faeae4646c288ca50faebb59e31834
Bug <khanhnd.B18CN309@stu.ptit.edu.vn>
2021-01-30 16:15:36 +0700

    update

--- src/components/InfoBar/InfoBar.js
+++ src/components/InfoBar/InfoBar.js
@@ -7,7 +7,7 @@ import onlineIcon from '../../icons/onlineIcon.png';
 const InfoBar = ({ room }) => (
     <div className="infoBar">
         <div className="leftInnerContainer">
-            <img className='onlineIcon' src={onlineIcon} />
+            <img className='onlineIcon' src={onlineIcon} alt="online"/>
             <h3>{room}</h3>
         </div>
         <div className='rightInnerContainer'>
